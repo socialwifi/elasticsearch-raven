0963ad37c5fa5b3e9ef24e35d419cf23f85fbfd1
Marek Siarkowicz <marek.siarkowicz@pozytywnie.pl>
2014-03-24 11:19:00 +0100

    Refactor udp_server imports.

--- elasticsearch_raven/udp_server.py
+++ elasticsearch_raven/udp_server.py
@@ -1,11 +1,11 @@
 import argparse
 import base64
-from datetime import datetime
+import datetime
 import os
-from queue import Queue
+import queue
 import socket
 import sys
-from threading import Thread
+import threading
 
 from elasticsearch_raven.transport import decode
 from elasticsearch_raven.transport import ElasticsearchTransport
@@ -30,7 +30,7 @@ def _run_server(ip, port):
         sys.stdout.write('Wrong hostname\n')
         return
 
-    blocking_queue = Queue(maxsize=os.environ.get('QUEUE_MAXSIZE', 1000))
+    blocking_queue = queue.Queue(maxsize=os.environ.get('QUEUE_MAXSIZE', 1000))
     sender = _get_sender(blocking_queue)
     sender.start()
     _serve(sock, blocking_queue)
@@ -42,7 +42,7 @@ def _serve(sock, blocking_queue):
         auth_header, data = data_with_header.split(b'\n\n')
         blocking_queue.put(base64.b64decode(data))
         sys.stdout.write('{host}:{port} [{date}]\n'.format(
-            host=addr[0], port=addr[1], date=datetime.now()))
+            host=addr[0], port=addr[1], date=datetime.datetime.now()))
 
 
 def _get_sender(blocking_queue):
@@ -54,7 +54,7 @@ def _get_sender(blocking_queue):
             data = blocking_queue.get()
             transport.send(decode(data))
             blocking_queue.task_done()
-    sender = Thread(target=_send)
+    sender = threading.Thread(target=_send)
     return sender
 
 
