217fe4e40e89b296147e1903c509be4f90024a89
Marek Siarkowicz <marek.siarkowicz@pozytywnie.pl>
2014-03-24 10:48:13 +0100

    Refactor requirements.

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
-from pip.req import parse_requirements
 
-requirements = parse_requirements('requirements.txt')
-install_requires = [str(requirement.req) for requirement in requirements]
 
 setup(
     name='Elasticsearch-Raven',
@@ -14,5 +11,6 @@ setup(
     license='LICENSE',
     description='Proxy that allows to log from raven to elasticsearch.',
     long_description=open('README.md').read(),
-    install_requires=install_requires,
+    install_requires=['elasticsearch'],
+    test_suite='tests'
 )
