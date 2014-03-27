4945c8962217128b2fddf64eb9ad2d26ab795ede
Tomek <t.pazurkiewicz@gmail.com>
2014-03-27 13:47:20 +0100

    Add url to setup.pu

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.1.1',
     author='Tomasz Wysocki',
     author_email='tomasz@pozytywnie.pl',
+    url='https://github.com/pozytywnie/elasticsearch-raven/',
     packages=['elasticsearch_raven'],
     scripts=['bin/elasticsearch-raven.py'],
     license='MIT',
