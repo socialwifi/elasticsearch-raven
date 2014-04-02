e7e9a99694ed59d65935161d0d328cfe0616bd0b
Marek Siarkowicz <marek.siarkowicz@pozytywnie.pl>
2014-04-02 09:53:47 +0200

    Refactor.

--- elasticsearch_raven/transport.py
+++ elasticsearch_raven/transport.py
@@ -17,19 +17,7 @@ elasticsearch_logger.setLevel(logging.ERROR)
 BaseMessage = collections.namedtuple('BaseMessage', ['headers', 'body'])
 
 
-class Message(BaseMessage):
-    @classmethod
-    def from_message(cls, message):
-        return cls(message.headers, message.body)
-
-    def decode_body(self):
-        return self.body
-
-
-class SentryMessage(Message):
-    def __init__(self, headers, body):
-        super().__init__(headers, body)
-
+class SentryMessage(BaseMessage):
     @classmethod
     def create_from_udp(cls, data):
         byte_headers, data = data.split(b'\n\n')
@@ -53,25 +41,18 @@ class SentryMessage(Message):
         return json.loads(zlib.decompress(self.body).decode('utf-8'))
 
 
-class ElasticsearchMessage(Message):
-    def __init__(self, headers, body):
-        super().__init__(headers, body)
-        postfix_encoded_data(self.body)
-
-
 class ElasticsearchTransport:
     def __init__(self, host, use_ssl=False):
         self._host = host
         self._use_ssl = use_ssl
 
     def send(self, message):
-        message = ElasticsearchMessage.from_message(message)
-        message_body = message.decode_body()
-        dated_index = message_body['project'].format(datetime.datetime.now())
+        postfix_encoded_data(message.body)
+        dated_index = message.body['project'].format(datetime.datetime.now())
         http_auth = '{}:{}'.format(message.headers['sentry_key'],
                                    message.headers['sentry_secret'])
         connection = elasticsearch.Elasticsearch(hosts=[self._host],
                                                  http_auth=http_auth,
                                                  use_ssl=self._use_ssl)
-        connection.index(body=message_body, index=dated_index,
+        connection.index(body=message.body, index=dated_index,
                          doc_type='raven-log')
