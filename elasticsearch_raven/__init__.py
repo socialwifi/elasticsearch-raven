d180268b2221950aa9498361b56ae7d8c96a4d66
Tomasz Pazurkiewicz <tomasz.pazurkiewicz@pozytywnie.pl>
2014-07-02 11:05:56 +0200

    Add amqp support.

--- elasticsearch_raven/__init__.py
+++ elasticsearch_raven/__init__.py
@@ -4,5 +4,8 @@ configuration = {
     'host': os.environ.get('ELASTICSEARCH_HOST', 'localhost:9200'),
     'use_ssl': os.environ.get('USE_SSL', False),
     'queue_maxsize': os.environ.get('QUEUE_MAXSIZE', 1000),
-    'http_auth': os.environ.get('ELASTICSEARCH_AUTH', None)
+    'http_auth': os.environ.get('ELASTICSEARCH_AUTH', None),
+    'amqp_url': os.environ.get('AMQP_URL',
+                               'amqp://guest:guest@localhost:5672//'),
+    'amqp_queue': os.environ.get('AMQP_QUEUE', 'elasticsearch-raven-logs'),
 }
