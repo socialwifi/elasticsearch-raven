ec8da62da6ecf7d5d86ca8bd9f33eb92a5b3df9e
Tomasz Pazurkiewicz <tomasz.pazurkiewicz@pozytywnie.pl>
2014-07-11 11:50:23 +0200

    Refactor.

--- elasticsearch_raven/udp_server.py
+++ elasticsearch_raven/udp_server.py
@@ -1,18 +1,15 @@
 import argparse
 import socket
 import sys
-import time
-import threading
 
 try:
     import queue
 except ImportError:
     import Queue as queue
 
-import elasticsearch
-
 from elasticsearch_raven import configuration
 from elasticsearch_raven import transport
+from elasticsearch_raven import queue_sender
 from elasticsearch_raven import queues
 from elasticsearch_raven import udp_handler
 
@@ -67,8 +64,8 @@ class Server(object):
         handler = udp_handler.Handler(
             self.sock, self.pending_logs, self.thread_exception_handler,
             debug=self.debug).as_thread()
-        sender = Sender(self.log_transport, self.pending_logs,
-                        self.thread_exception_handler).as_thread()
+        sender = queue_sender.Sender(self.log_transport, self.pending_logs,
+                                     self.thread_exception_handler).as_thread()
         handler.start()
         sender.start()
         try:
@@ -86,61 +83,3 @@ class Server(object):
 
     def thread_exception_handler(self, exception):
         self.exception_queue.put(exception)
-
-
-class Sender(object):
-    def __init__(self, log_transport, pending_logs, exception_handler):
-        self.log_transport = log_transport
-        self.pending_logs = pending_logs
-        self.exception_handler = exception_handler
-
-    def as_thread(self):
-        sender = threading.Thread(target=self._send)
-        sender.daemon = True
-        return sender
-
-    def _send(self):
-        try:
-            while True:
-                self._send_message()
-        except Exception as e:
-            self.exception_handler(e)
-
-    def _send_message(self):
-        message = self.pending_logs.get()
-        try:
-            for retry in retry_loop(15 * 60, delay=1, back_off=1.5):
-                try:
-                    self.log_transport.send_message(message)
-                except elasticsearch.exceptions.ConnectionError as e:
-                    retry(e)
-        except elasticsearch.exceptions.TransportError as e:
-            self._raport_error(message, e)
-        self.pending_logs.task_done()
-
-    def _raport_error(self, message, error):
-        connection = elasticsearch.Elasticsearch(
-            hosts=[configuration['host']],
-            use_ssl=configuration['use_ssl'],
-            http_auth=configuration['http_auth'])
-        body = {'message': str(message), 'error': str(error)}
-        connection.index(index='elasticsearch-raven-error', body=body,
-                         doc_type='elasticsearch-raven-log')
-
-
-def retry_loop(timeout, delay, back_off=1.0):
-    start_time = time.time()
-    exceptions = set()
-
-    def retry(exception):
-        exceptions.add(exception)
-    yield retry
-    while time.time() - start_time <= timeout:
-        if not exceptions:
-            return
-        time.sleep(delay)
-        delay *= back_off
-        exceptions.clear()
-        yield retry
-
-    raise exceptions.pop()
