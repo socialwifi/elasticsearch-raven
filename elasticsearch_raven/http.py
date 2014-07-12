ff0021e824476f036fb6a7978679e2f5413b16b6
Tomasz Wysocki <tomasz@wysocki.info>
2014-07-13 00:16:38 +0200

    Refactor.

--- elasticsearch_raven/http.py
+++ elasticsearch_raven/http.py
@@ -35,5 +35,5 @@ class HttpUtils:
             status = '200 OK'
             response_headers = [('Content-Type', 'text/plain')]
             start_response(status, response_headers)
-            return [''.encode('utf-8')]
+            return [b'']
         return application
